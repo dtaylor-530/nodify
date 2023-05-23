c221bfa317a9e9f1360fa1b689444cfe2b891edc
dtaylor-530 <d.taylor530@gmail.com>
2023-05-23 13:00:54 +0100

    Improves view of Connectors (i.e inputs/outputs)

---
+++ Nodify.Demo/Infrastructure/TypeConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Nodify.Demo
+{
+    public class TypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return value.GetType().Name;
+            }
+            catch
+            {
+                return DependencyProperty.UnsetValue;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static TypeConverter Instance { get; } = new();
+    }
+}
